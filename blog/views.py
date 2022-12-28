6716c6e3e8b331bd700c4b69b5d4dbec81021fe7
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-12-28 22:23:09 +0100

    adding views for list and details view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404('Post does not exist')
+
+    return render(request, 'blog/post/detail.html', {'post': post})
