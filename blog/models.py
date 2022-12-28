260c05c80507f9d2353c79f3c509e350471bffd6
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-12-28 22:16:35 +0100

    updated models

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -21,12 +26,14 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=2, choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['publish']),
-        ]
+        # indexes = [
+        #     models.Index(fields=['publish']),
+        # ]
 
     def __str__(self):
         return self.title
