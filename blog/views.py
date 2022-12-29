91070d7dd0744c4bfb09153a655bcfdfed0a93c3
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-12-29 12:40:17 +0100

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 
 def post_list(request):
     posts = Post.published.all()
+    # pagination with 5 post per page
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.get_page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
