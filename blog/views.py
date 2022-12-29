cb5618318a30a5a1f7ae7774c201a7a9e1fa9a9f
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-12-29 12:51:30 +0100

    trying class based views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
