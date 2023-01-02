a2dc182754daa18ebc6246f7c1228f65b4c25778
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-01-03 00:36:15 +0100

    retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
@@ -44,8 +45,14 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # form for user to add comments
     form = CommentForm()
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:3]
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
