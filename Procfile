1a64b64967c09075ffe8528a7117f4002fafc9c6
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2023-02-04 15:30:54 +0100

    deploying to railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'techBlog.wsgi
--- techBlog/settings.py
+++ techBlog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -135,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
